a0813a616e4c37ecf2df703ecf073ee04a095f03
Hiddestroyer <hidde.weide@student.hu.nl>
2026-03-09 10:31:03 +0100

    css voor inlog en register

--- public/register.js
+++ public/register.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('form');
-
+    
     form.addEventListener('submit', async (e) => {
         e.preventDefault();
 
